a25c3354de6b04f6ba370ad4f6fd3afcda3360c6
Anastasiia Lekvina <146827165+AnastasiiaLevkina@users.noreply.github.com>
2024-10-28 15:52:12 +0100

    Terminado.

--- main.ts
+++ main.ts
@@ -1,3 +1,25 @@
-basic.forever(function () {
-	
+radio.onReceivedNumber(function (receivedNumber) {
+    basic.showNumber(receivedNumber)
+    basic.pause(1000)
+    if (dice_throw > receivedNumber) {
+        basic.showIcon(IconNames.Happy)
+    } else {
+        basic.showIcon(IconNames.Sad)
+    }
 })
+input.onButtonPressed(Button.A, function () {
+    radio.sendString("Hola!")
+})
+input.onButtonPressed(Button.AB, function () {
+    radio.sendNumber(dice_throw)
+})
+radio.onReceivedString(function (receivedString) {
+    basic.showString(receivedString)
+})
+input.onButtonPressed(Button.B, function () {
+    dice_throw = randint(1, 6)
+    basic.showNumber(dice_throw)
+    basic.pause(1000)
+})
+let dice_throw = 0
+radio.setGroup(1)
